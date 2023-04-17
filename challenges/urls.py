48958dc73ed769cc176f220db87d2265d3cd5a1b
KramStyles <kramstyles@outlook.com>
2023-04-17 08:26:46 +0100

    added 404 page

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,5 +4,5 @@ from challenges import views
 urlpatterns = [
     path("<int:day>", views.daily_challenge_by_number),
     path("<str:day>", views.index, name="weekly-activities"),
-    path("", views.challenge_home),
+    path("", views.challenge_home, name="challenges-home"),
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,29 +1,36 @@
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import reverse, redirect
+from django.shortcuts import reverse, redirect, render
 
 # Create your views here.
 
 daily_challenges = {
-        "monday": "Make weekly plans",
-        "tuesday": "Reflect on previous day",
-        "wednesday": "Read 5 books",
-        "thursday": "Visit your friends",
-        "friday": "Play games",
-        "saturday": "Build your empire",
-        "sunday": "Think and worry less",
-    }
+    "monday": "Make weekly plans",
+    "tuesday": "Reflect on previous day",
+    "wednesday": "Read 5 books",
+    "thursday": "Visit your friends",
+    "friday": "Play games",
+    "saturday": "Build your empire",
+    "sunday": "Think and worry less",
+}
+
 
 def challenge_home(request):
     iterated_list = ""
+    context = {"title": "welcome"}
 
     for day in daily_challenges:
-        iterated_list += f"<li><a href='{reverse('weekly-activities', args=[day])}'>{day.title()}</a></li>"
+        iterated_list += f"""<li class='list-group-item d-grid'>
+            <a href='{reverse('weekly-activities', args=[day])}'
+            class='p-1'>{day.title()}</a>
+            </li>"""
     response_data = f"""
-        <ul>
+        <ul class='list-group'>
             {iterated_list}
         </ul>
     """
-    return HttpResponse(response_data)
+    context["data"] = response_data
+    return render(request, "challenges/home.html", context=context)
+
 
 def index(request, day):
     # result = daily_challenges.get(month.lower()) or "Month not listed"
@@ -33,12 +40,16 @@ def index(request, day):
         return HttpResponseNotFound("<h2>Day not supported!</h2>")
     return HttpResponse(f"<h2>{result}</h2>")
 
+
 def daily_challenge_by_number(request, day):
     # Here we will redirect to the appropriate day
     week = list(daily_challenges.keys())
     try:
-        day = week[day-1]
+        day = week[day - 1]
         return redirect("weekly-activities", day=day)
     except IndexError as err:
-        print(err)
-        return HttpResponseNotFound(f"<h2>{err}</h2>")
+        context = {
+            'title': "page not found",
+            "reason": str(err)
+        }
+        return render(request, "challenges/404.html", context)
