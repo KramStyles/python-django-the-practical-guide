d84e40cad039b65ea33255bbf66fa46110252104
KramStyles <kramstyles@outlook.com>
2023-04-17 16:17:43 +0100

    finally worked with cycle, with in for

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from blog import views
+
+urlpatterns = [
+    path("", views.index, name="blog-home"),
+    path("posts/", views.post, name="blog-post"),
+    path("posts/<slug>/", views.post, name="blog-post-details"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+def index(request):
+    context = {"title": "News"}
+    return render(request, "blog/index.html", context)
+
+
+def post(request):
+    return redirect("blog-home")
+
+
+def post_details(request):
+    pass
