6a36fa67b9256b350e9fce048888a009a704dfc7
kramstyles <kramstyles@outlook.com>
2023-04-22 11:22:04 +0100

    created index for book app

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from book.models import Book
+
+
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    context = {"title": "book list", "books": books}
+    return render(request, "book/index.html", context)
