d55dcae78fd9375b697e669c3e1e100eecb6d638
kramstyles <kramstyles@outlook.com>
2023-04-22 18:16:18 +0100

    made author a model and foreign key

--- book/admin.py
+++ book/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 
-from book.models import Book
+from book.models import Book, Author
 
 
 class BookAdmin(admin.ModelAdmin):
     # Taking this out because of pre-population
     # readonly_fields = ("slug", )
     prepopulated_fields = {"slug": ("title", "author")}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
 
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
--- book/models.py
+++ book/models.py
@@ -6,12 +6,20 @@ from django.utils.text import slugify
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT, null=True)
     is_bestselling = models.BooleanField(default=False, verbose_name="Is Best-Seller?")
 
     # Adding editable = False would hide the field in the admin
