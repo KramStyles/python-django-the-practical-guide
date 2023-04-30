1b15d56a029349144e32a0fa3223b78a67eb7b21
kramstyles <kramstyles@outlook.com>
2023-04-30 22:16:33 +0100

    manually insert form

--- challenges/forms.py
+++ challenges/forms.py
@@ -2,5 +2,20 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    username = forms.CharField(max_length=100, required=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fields in self.visible_fields():
+            fields.field.widget.attrs["class"] = "form-control my-1"
+
+    username = forms.CharField(
+        max_length=100,
+        required=True,
+        label="User Name",
+        error_messages={
+            "required": "Username is important.",
+            "max_length": "Maximum of 100 Characters exceeded.",
+        },
+    )
     password = forms.CharField(min_length=4, required=True)
+    review_text = forms.CharField(widget=forms.Textarea, required=False)
+    ratings = forms.IntegerField(min_value=1, max_value=5, label="Your Ratings")
